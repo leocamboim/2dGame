18473a38e7c69f67746daa725dffc8608382cb9a
Leonardo Camboim <leonardoribeirocamboim@gmail.com>
2020-05-29 20:51:38 -0300

    Health System
    Health system and bar scroll as the player takes damage

--- Assets/Codes/PlayerHealth.cs
+++ Assets/Codes/PlayerHealth.cs
@@ -4,32 +4,15 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public GameObject deathFXPrefab;
-
-    bool isAlive = true;
-    int trapsLayer;
-    // Start is called before the first frame update
+    //public Transform pfHealthBar;
+    public HealthBar healthBar;
     void Start()
     {
-        trapsLayer = LayerMask.NameToLayer("Traps");
-    }
+        HealthSystem healthSystem = new HealthSystem(100);
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer != trapsLayer || !isAlive)
-            return;
+        //Transform healthBarTransform = Instantiate(pfHealthBar, new Vector3(0, 10), Quaternion.identity);
+        //HealthBar pfhealthBar = healthBarTransform.GetComponent<HealthBar>();
+        healthBar.Setup(healthSystem);
 
-        isAlive = false;
-
-        Instantiate(deathFXPrefab, transform.position, transform.rotation);
-
-        gameObject.SetActive(false);
-
-        //GameManeger.PlayerDied();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
